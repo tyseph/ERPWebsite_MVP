152a2ffb7f4343f7602993feac596c134b9e3ec0
tyseph <sg748@snu.edu.in>
2021-05-26 02:21:38 +0530

    Final upload

--- ERPAssitantWerking-master/js/app.js
+++ ERPAssitantWerking-master/js/app.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAitRx6veJTXjFa3r-JARhAzMhivVHS2wo",
-    authDomain: "crud-c25d0.firebaseapp.com",
-    databaseURL: "https://crud-c25d0.firebaseio.com",
-    projectId: "crud-c25d0",
-    storageBucket: "crud-c25d0.appspot.com",
-    messagingSenderId: "376741018743",
-    appId: "1:376741018743:web:c5e38b935db297a9bdda72"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyACL51Js5SRZfPO6HlZ6Y0WoH1JvWRw9m4",
+  authDomain: "sajal-411ac.firebaseapp.com",
+  databaseURL: "https://sajal-411ac-default-rtdb.firebaseio.com/",
+  projectId: "sajal-411ac",
+  storageBucket: "sajal-411ac.appspot.com",
+  messagingSenderId: "327821682589",
+  appId: "1:327821682589:web:0f40e7d15f9b49e06a3174"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
